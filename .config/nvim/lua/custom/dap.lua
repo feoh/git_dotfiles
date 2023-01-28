271e6b970bcb39a0052d6ea072b6f6ece71e4915
Chris Patti <feoh@feoh.org>
2023-01-28 16:59:55 -0500

    Add Conjure

--- .config/nvim/lua/custom/dap.lua
+++ .config/nvim/lua/custom/dap.lua
@@ -1,5 +1,6 @@
 
   require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+  require('dap-python').test_runner = 'pytest'
   require("nvim-dap-virtual-text").setup {
     enabled = true,
     commented = true,
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,10 +1,11 @@
 return function(use)
-  use {"ellisonleao/glow.nvim"}
+  use {'ellisonleao/glow.nvim'}
   use {'ixru/nvim-markdown'}
   use {'mfussenegger/nvim-dap'}
   use {'mfussenegger/nvim-dap-python'}
   use {'theHamsta/nvim-dap-virtual-text'}
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use {'rcarriga/nvim-dap-ui', requires = {"mfussenegger/nvim-dap"}}
+  use {'Olical/conjure'}
 
   require("custom.dap")
 end
