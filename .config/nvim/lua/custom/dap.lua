e6f41a451a1909d13d5d1b7d34b0c181f1e5c7e0
Christopher Patti <cpatti@rocinante.localdomain>
2023-02-17 11:10:01 -0500

    Switch to new lazy.nvim version of kickstart. Will re-integrate bits from defaults.lua later.

--- .config/nvim/lua/custom/dap.lua
+++
@@ -1,8 +0,0 @@
-
-  require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
-  require('dap-python').test_runner = 'pytest'
-  require("nvim-dap-virtual-text").setup {
-    enabled = true,
-    commented = true,
-  }
-  require("dapui").setup()
