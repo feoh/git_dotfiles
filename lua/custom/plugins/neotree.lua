91a207ac01eb7b470ac89d1451c239c716aae1f2
Chris Patti <feoh@feoh.org>
2023-07-29 12:17:05 -0400

    Update neotree to override netrw

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -3,7 +3,49 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-  }
+  },
+  opts = {
+    window = {
+      position = "left",
+      width = 40,
+      mapping_options = {
+        noremap = true,
+        nowait = true,
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          "node_modules"
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          --".DS_Store",
+          --"thumbs.db"
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
+      follow_current_file = {
+        enabled = false,                      -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false,              -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      group_empty_dirs = false,               -- when true, empty folders will be grouped together
+      hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+    },
+  },
 }
