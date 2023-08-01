0d7a8da09db798ba8738d44bf19bd79fc3cc1835
Chris Patti <feoh@feoh.org>
2023-07-31 20:20:52 -0400

    Switch to relative line numbers

--- init.lua
+++ init.lua
@@ -217,6 +217,9 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Try relative line numbers
+vim.wo.relativenumber = true
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
