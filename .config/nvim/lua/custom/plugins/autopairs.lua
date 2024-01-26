bb656b524ccfbe3bf27b11bc0aaedacc6d84efef
Chris Patti <feoh@feoh.org>
2024-01-26 01:36:52 -0500

    Fix: properly configure autopairs.

--- .config/nvim/lua/custom/plugins/autopairs.lua
+++ .config/nvim/lua/custom/plugins/autopairs.lua
@@ -1,6 +1,7 @@
 return {
-	"windwp/nvim-autopairs",
-	config = function()
-		require("nvim-autopairs").setup {}
-	end,
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
+	opts = {
+		fast_wrap = {},
+	} -- this is equalent to setup({}) function
 }
