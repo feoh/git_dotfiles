38dc54e3bf55d4eb247edc8ec3f2c6681441e8af
Chris Patti <feoh@feoh.org>
2024-01-26 15:07:37 -0500

    Fix: proper firenvim config to not always kick in

--- .config/nvim/lua/custom/plugins/firenvim.lua
+++ .config/nvim/lua/custom/plugins/firenvim.lua
@@ -5,6 +5,16 @@ return {
 	-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
 	lazy = not vim.g.started_by_firenvim,
 	build = function()
+		require("lazy").load({ plugins = "firenvim", wait = true })
 		vim.fn["firenvim#install"](0)
+	end,
+	config = function()
+		vim.g.firenvim_config = {
+			localSettings = {
+				[".*"] = {
+					takeover = "never",
+				}
+			}
+		}
 	end
 }
