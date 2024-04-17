dc38c2d61b269447688debaf6f10595bdab61dfc
Chris Patti <feoh@feoh.org>
2024-04-17 11:19:25 -0400

    Fix: fix spellfile location and add snobol to dict

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -5,4 +5,4 @@ vim.opt.colorcolumn = '+1'
 -- Spelling
 vim.opt_local.spell = true
 vim.opt_local.spelllang = 'en_us'
-vim.opt_local.spellfile = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
+vim.opt_local.spellfile = vim.fn.finddir '~/.config' .. '/spell/en.utf-8.add'
