3884c01459ad6a2fa857b15f779d181b75cc5e31
Chris Patti <feoh@feoh.org>
2024-11-07 10:57:56 -0500

    Turn on linting and image passthrough for tmux.

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -11,8 +11,14 @@ return {
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft = {
+        markdown = { 'vale' },
+        text = { 'vale' },
+        dockerfile = { 'hadolint' },
+        rst = { 'rstcheck' },
+        python = { 'ruff' },
+      }
       --
       -- However, note that this will enable a set of default linters,
       -- which will cause errors unless these tools are available:
