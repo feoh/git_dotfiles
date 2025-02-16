71850f494d196385c78c58c18e07d652090fa581
Chris Patti <feoh@feoh.org>
2025-02-16 10:59:05 -0500

    Hard code the Python DAP debugger so we don't need to faff with Mason.

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -23,6 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'mfussengger/nvim-dap-python',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
