97c865c620938828f4eb9f218dcfce658b05da4f
Chris Patti <feoh@feoh.org>
2025-02-17 11:34:56 -0500

    Add telescope-fzf-native.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,17 @@
 -- plugins/telescope.lua:
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+     {
+       'nvim-telescope/telescope-fzf-native.nvim',
+       build = 'make',
+       -- installed and loaded.
+       cond = function()
+         return vim.fn.executable 'make' == 1
+       end,
+     },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'echasnovski/mini.icons', version = '*' },
+  }
 }
