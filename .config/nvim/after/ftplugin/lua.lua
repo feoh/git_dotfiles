2e1b8e8df6dc7664e55f15fc0c09a0773534b75c
Chris Patti <feoh@feoh.org>
2025-02-18 17:41:40 -0500

    Fix bogus llm error

---
+++ .config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+-- Set tab width to 2 spaces
+vim.opt.tabstop = 2         -- Number of spaces that a tab counts for
+vim.opt.shiftwidth = 2      -- Size of an autoindented shift
+vim.opt.expandtab = true     -- Use spaces instead of tabs
---
+++ .config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,4 @@
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4         -- Number of spaces that a tab counts for
+vim.opt.shiftwidth = 4      -- Size of an autoindented shift
+vim.opt.expandtab = true     -- Use spaces instead of tabs
