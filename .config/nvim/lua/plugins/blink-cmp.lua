0e3b6b57fceb2c256f313523f8b73a2869a5b449
Chris Patti <feoh@feoh.org>
2025-02-18 17:47:03 -0500

    Fix utterly BOGUS blink-cmp-copilot config.

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -18,20 +18,8 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
     keymap = { preset = 'default' },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = false,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'normal',
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lazydev', 'lsp', 'copilot', 'path', 'snippets', 'buffer' },
-      },
+    sources = {
+      default = { 'lazydev', 'lsp', 'copilot', 'path', 'snippets', 'buffer' },
       providers = {
         lazydev = {
           name = "LazyDev",
@@ -48,6 +36,15 @@ return {
       },
     },
     appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- Will be removed in a future release
+      use_nvim_cmp_as_default = false,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'normal',
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
       kind_icons = {
         Copilot = "",
       },
