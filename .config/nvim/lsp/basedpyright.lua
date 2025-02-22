bcaabdb6344621d2d3293ed4fd93c6eac3517a28
Chris Patti <feoh@feoh.org>
2025-02-21 23:35:12 -0500

    Switch to basedpyright for Python LSP.

---
+++ .config/nvim/lsp/basedpyright.lua
@@ -0,0 +1,39 @@
+---@type vim.lsp.Config
+return {
+    cmd = { "basedpyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    -- ---@param client vim.lsp.Client
+    -- on_attach = function(client)
+    --     --
+    --     -- Use treesitter highlighting, as it supports injections.
+    --     if client.server_capabilities then
+    --         client.server_capabilities.semanticTokensProvider = nil
+    --     end
+    -- end,
+    root_markers = {
+        "Pipfile",
+        "pyproject.toml",
+        "pyrightconfig.json",
+        "requirements.txt",
+        "setup.cfg",
+        "setup.py",
+    },
+    settings = {
+        basedpyright = {
+            analysis = {
+                autoImportCompletions = true,
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                reportMissingTypeStubs = true,
+                reportUndefinedVariable = "none", -- covered by ruff
+                reportUnreachable = "none",
+                reportUnusedImport = "none", -- covered by ruff
+                typeCheckingMode = "basic", -- standard
+                useLibraryCodeForTypes = true,
+            },
+            disableOrganizeImports = false,
+        },
+    },
+    single_file_support = true,
+}
+
