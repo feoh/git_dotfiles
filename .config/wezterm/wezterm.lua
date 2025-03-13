b9da5e2a5b35a9197849f79439ac1ff12f65d2b2
Christopher Patti <feoh@feoh.org>
2025-03-13 11:50:53 -0400

    Maximize WezTerm window and use a font I can read

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,44 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = "Dracula"
-config.font = wezterm.font("Firacode Nerd Font")
-config.font_size = 18
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 20
 
-print(config)
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "AdventureTime"
+config.color_scheme = "Dark Pastel (Gogh)"
+
+-- WSL first please!
+config.default_prog = { "ubuntu" }
+
+config.keys = {
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.launch_menu = {
+		{
+			args = { "ubuntu" },
+		},
+		{
+			args = { "pwsh" },
+		},
+	}
+end
+
+local seen = {}
+wezterm.on("window-focus-changed", function(window, pane)
+	local wid = window:window_id()
+	if not seen[wid] then
+		seen[wid] = true
+		window:maximize()
+	end
+end)
+
+-- and finally, return the configuration to wezterm
 return config
